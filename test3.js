0ed3cef0a86765e140d0aad5d8c2467fa237ca7e
Rami Barakat <Barakat.Rami@outlook.com>
2023-01-28 04:06:12 -0600

    tried Dan Schroeder's approach to stream particles

--- test3.js
+++ test3.js
@@ -3,9 +3,9 @@ const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 
 canvas.width = 800;
-canvas.height = 100;
+canvas.height = 250;
 
-var pxPerSquare = 4;
+var pxPerSquare = 5;
 
 var xdim = canvas.width / pxPerSquare;
 var ydim = canvas.height / pxPerSquare;
@@ -26,10 +26,10 @@ const w = [
 ];
 
 const findex = [5, 6, 7, 8, 1, 2, 3, 4];
-const Re = 250;
+const Re = 100;
 
-let obsRadius = 9;                          // obstacale properties 
-let obsXpos = Math.round(xdim * 0.9);
+let obsRadius = 5;                          // obstacale properties 
+let obsXpos = Math.round(xdim * 0.1);
 let obsYpos = Math.round(ydim * 0.4);
 
 // let obsRadius2 = 10;                          // obstacale properties 
@@ -37,11 +37,11 @@ let obsYpos = Math.round(ydim * 0.4);
 // let obsYpos2 = Math.round(ydim*0.5);
 
 
-const deltaT = 5;                             // time step
+const deltaT = 10;                             // time step
 let ux0 = 0.2;                                // initial x velocity 
 let uy0 = 0;                                  // initial y velocity 
 let rho = 1;                                  // initial density 
-const mu = ux0 * obsRadius / Re;	                          // viscosity
+const mu = 0.02;	                          // viscosity
 const omega = 1 / (3 * mu + 0.5);
 
 const Cell = (rho, ux, uy, isObstacle) => {
@@ -83,16 +83,20 @@ function relax(ux, uy, rho) {
     }
     return f;
 }
-function setObstacle(radius, xpos, ypos) {
-    for (let i = xpos; i < xpos + 1; i++) {
-        for (let j = ypos; j < ypos + radius; j++) {
-            grid[IX(i, j)].isObstacle = true;
-        }
+function setObstacle(radius, xpos, ypos, startAngle, endAngle) {
+    for (let angle = startAngle; angle < endAngle; angle += 0.01) {
+        let x = xpos + radius * Math.cos(angle);
+        let y = ypos + radius * Math.sin(angle);
+        grid[IX(Math.round(x), Math.round(y))].isObstacle = true;
     }
 }
 
-setObstacle(obsRadius, obsXpos, obsYpos);
+
+
+setObstacle(obsRadius, obsXpos, obsYpos, 5, 7);
+
 // setObstacle(obsRadius2, obsXpos2, obsYpos2);
+
 
 
 
@@ -105,7 +109,7 @@ for (let i = 0; i < xdim; i++) {
 function collide() {
     for (let i = 0; i < xdim; i++) {
         for (let j = 0; j < ydim; j++) {
-            let index = IX(i, j);
+            let index = IX(i, j);		// array index for this lattice site
             grid[index].rho = sumMatrix(grid[index].fi);
             grid[index].ux = 0;
             grid[index].uy = 0;
@@ -117,38 +121,55 @@ function collide() {
             for (let n = 0; n < 9; n++) {
                 grid[index].fi[n] += omega * (feq[n] - grid[index].fi[n]);
             }
+            // fout[index] = [...grid[index].fi];
         }
     }
 }
 
 function stream() {
-    let temp_grid = _.cloneDeep(grid);
-    for (let y = 1; y < ydim-1; y++) {
-        for (let x = 1; x < xdim-1; x++) {
-            for (let i = 0; i < 9; i++) {
-                let posx = (x + e[i][0] ) ;
-                let posy = (y + e[i][1]);
-                grid[IX(x, y)].fi[i] = temp_grid[IX(posx, posy)].fi[i];
+
+    for (var y = ydim - 2; y > 0; y--) {
+        for (var x = 1; x < xdim - 1; x++) {
+            grid[IX(x, y)].fi[1] = grid[IX(x, y - 1)].fi[1];
+            grid[IX(x, y)].fi[8] = grid[IX(x + 1, y - 1)].fi[8];
+        }
+    }
+    for (var y = ydim - 2; y > 0; y--) {
+        for (var x = xdim - 2; x > 0; x--) {
+            grid[IX(x, y)].fi[3] = grid[IX(x - 1, y)].fi[3];
+            grid[IX(x, y)].fi[2] = grid[IX(x - 1, y - 1)].fi[2];
+        }
+    }
+    for (var y = 1; y < ydim - 1; y++) {
+        for (var x = xdim - 2; x > 0; x--) {
+            grid[IX(x, y)].fi[5] = grid[IX(x, y + 1)].fi[5];
+            grid[IX(x, y)].fi[4] = grid[IX(x - 1, y + 1)].fi[4];
+        }
+    }
+    for (var y = 1; y < ydim - 1; y++) {
+        for (var x = 1; x < xdim - 1; x++) {
+            grid[IX(x, y)].fi[7] = grid[IX(x + 1, y)].fi[7];			// move the west-moving particles
+            grid[IX(x, y)].fi[6] = grid[IX(x + 1, y + 1)].fi[6];		// and the southwest-moving particles
+        }
+    }
+    for (var y = 1; y < ydim - 1; y++) {				// Now handle bounce-back from barriers
+        for (var x = 1; x < xdim - 1; x++) {
+            if (grid[x + y * xdim].isObstacle) {
+                var index = x + y * xdim;
+                grid[IX(x + 1, y)].fi[2] = grid[index].fi[8];
+                grid[IX(x - 1, y)].fi[8] = grid[index].fi[2];
+                grid[IX(x, y + 1)].fi[1] = grid[index].fi[5];
+                grid[IX(x, y - 1)].fi[5] = grid[index].fi[1];
+                grid[IX(x + 1, y + 1)].fi[2] = grid[index].fi[6];
+                grid[IX(x - 1, y + 1)].fi[8] = grid[index].fi[4];
+                grid[IX(x + 1, y - 1)].fi[4] = grid[index].fi[8];
+                grid[IX(x - 1, y - 1)].fi[6] = grid[index].fi[2];
+                // Keep track of stuff needed to plot force vector:
             }
         }
     }
-}
 
-function applyBoundaryConditions() {
-    for (let y = 1; y < ydim-1; y++) {
-        for (let x = 1; x < xdim-1; x++) {
-            if (grid[IX(x,y)].isObstacle) {
-                grid[x + y * xdim].fi[2] = grid[x + y * xdim].fi[8];
-                grid[x + y * xdim].fi[4] = grid[x + y * xdim].fi[6];
-                grid[x + y * xdim].fi[5] = grid[x + y * xdim].fi[1];
-                grid[x + y * xdim].fi[8] = grid[x + y * xdim].fi[2];
-                grid[x + y * xdim].fi[6] = grid[x + y * xdim].fi[4];
-                grid[x + y * xdim].fi[1] = grid[x + y * xdim].fi[5];
-            }
-        }
-    }
 }
-
 
 
 let colors = [];
@@ -204,8 +225,8 @@ let tempGrid = _.cloneDeep(grid);
 function simulate() {
     for (let i = 0; i < deltaT; i++) {
         collide();
+        // tempGrid = _.cloneDeep(grid);
         stream();
-        applyBoundaryConditions();
     }
 
     draw();
